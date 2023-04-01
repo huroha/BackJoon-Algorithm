d5a0d604ca9bceaa0274d47f7f2f83da9458ae86
Minseok Kwon <kmsms4050@gmail.com>
2023-04-01 14:00:23 +0900

    문자열

---
+++ 27866.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+// 문자와 문자열
+int main() {
+  
+   string word;
+   cin >> word;
+   int a;
+   cin >> a;
+   cout << word[a-1];
+   return 0;
+
+}
